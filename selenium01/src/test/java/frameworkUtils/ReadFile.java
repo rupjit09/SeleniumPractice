4b0a44e234449f728a9297b70fdf7dff3895b759
Rupjit <rdas1@zaloni.com>
2020-07-19 00:55:20 +0530

    Saving new learnings

--- selenium01/src/test/java/frameworkUtils/ReadFile.java
+++ selenium01/src/test/java/frameworkUtils/ReadFile.java
@@ -14,7 +14,7 @@ public class ReadFile {
 		
 		Properties prop= new Properties();
 		System.out.println(System.getProperty("user.dir")+"\\src\\test\\java\\frameworkUtils\\config.properties");
-		FileInputStream fp=new FileInputStream("C:/Users/rdas/Desktop/selenium/gitrepo/Learnselenium/selenium01/src/test/java/frameworkUtils/config.properties");
+		FileInputStream fp=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\java\\frameworkUtils\\config.properties");
 		prop.load(fp);
 		String propertyValue=prop.getProperty(propertyName, "Property Not Found");
 		return propertyValue;
